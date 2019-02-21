18c1a1dfdc3afdf103f8c75f2e355a9122fd6259
Brittany Deventer <bdvntr@gmail.com>
2019-02-20 23:12:58 -0800

    npm run start is not working... need to revert back

--- server/src/lib/server.js
+++ server/src/lib/server.js
@@ -8,33 +8,34 @@ import bodyParser from 'body-parser';
 const app = express();
 const router = express.Router();
 
-//env variables
+// env variables
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern-starter';
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-app.use(bodyParser.json(), cors());
+app.use(bodyParser.json(),cors())
+
 app.use(require('../route/auth-router'));
 
-app.all('x', (request, response) => {
-    console.log('Returning a 404 from the catch-all route');
-    return response.sendStatus(404);
+app.all('*', (request, response) => {
+  console.log('Returning a 404 from the catch-all route');
+  return response.sendStatus(404);
 });
 
-//error middleware
+// error middleware
 app.use(require('./error-middleware'));
 
+
 export const start = () => {
-    app.listen(PORT, () => {
-        console.log('Listening on port: ${PORT}')
-    })
-};
+  app.listen(PORT, () =>{
+    console.log(`Listening on port: ${PORT}`)
+  })
+}
 
 export const stop = () => {
-    app.close(PORT, () => {
-        console.log('Shut down on port: ${PORT}')
-    })
-};
-
+  app.close(PORT, () => {
+    console.log(`Shut down on port: ${PORT}`)
+  })
+}
