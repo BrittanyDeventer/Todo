99c142266a077d46a93f8720055d6d914a641ac3
Brittany Deventer <bdvntr@gmail.com>
2019-03-04 13:14:19 -0800

    express server connected to mlab

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000; //run on port 5000 until we deploy
 
